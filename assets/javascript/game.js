0dd66e165a09f4a506ecfc9777d571d1168b95da
Daniel Homann <danielhomann4@gmail.com>
2018-11-09 12:50:00 -0800

    got guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,37 @@
-var wins = 0;
-var losses = 0;
+
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
 'm','n','o','p','q','r','s','t','u','v','w','y','z'];
+var guesses = ['1','2','3','4','5','6','7','8','9'];
+var wins = "";
+var losses = "";
+var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+console.log(computerGuess)
 
-document.onkeyup = function(event) {
+
+
+document.onkeyup = function(event){
     var userGuess = event.key;
-    var computerGuess = alaphabet[Math.florr(Math.random() * alphabet.length)];
+    var guessed = userGuess;
 
+    if(userGuess === computerGuess){
+        wins++;
+
+    }else{
+        guesses--;
+    }
+
+    if(guesses === 0){
+        losses++
+    }
+
+    var html =
+    // "<p>Pyschic Game</p>"
+    // "<p>You chose: " + userGuess + "</p>" +
+    // "<p>The computer chose: " + computerGuess+ "</p>" +
+    "<p>Wins: " + wins + "</p>" +
+    "<p>Losses: " + losses + "</p>" +
+    "<p>Guesses Left: " + guesses + "</p>"+
+    "<p>Your Guesses so far: " + guessed + "</p>";
+
+    document.querySelector("#game").innerHTML = html;
 }
